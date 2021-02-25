367210a6265ad413eaf6749a80604e12f0e65c57
Nonagod <nonagod.public@gmail.com>
2021-02-25 14:48:42 +0300

    Added ManyActionAbstractionInterface

---
+++ src/ManyActionAbstractionInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace Nonagod\Abstractions;
+
+interface ManyActionAbstractionInterface {
+    /**
+     * Method using for call same action many times
+     *
+     * @param string $action_name - action name (depends from abstraction)
+     * @param array $action_parameters - parameters for each action call. array item must be array.
+     * @return array - array of a action results with action call parameters
+     */
+    public function doManyAction( string $action_name, array $action_parameters ) : array;
+}
